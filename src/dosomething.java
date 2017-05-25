5106e7fe17cd7b049920387f82c18962c239109b
ALESANDRO Marques <alesandro.marques@ibm.com>
2017-05-25 16:02:24 -0400

    java class ex

--- src/dosomething.java
+++ src/dosomething.java
@@ -1,9 +1,35 @@
+
+import java.util.Scanner;
 public class dosomething {
 
     public static void main(String[] args) {
-    people p = new people("Steve", "warden", 80000);
-    p.display();
-        people p2 = new people("john", "warden", 70000);
-        p2.display();
+
+    people [] parray = new people[5];
+    parray = fillpeopleArray(parray);
+    displayPeople(parray);
     }
+    public static people [] fillpeopleArray(people [] parray ){
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < parray.length ; i++) {
+            System.out.println("enter name");
+            String name = scan.nextLine();
+             name = scan.nextLine();
+            System.out.println("enter location");
+            String loc = scan.nextLine();
+            System.out.println("enter salary");
+            int sal = scan.nextInt();
+            parray[i]= new people(name,loc,sal);
+        }
+        return parray;
+
+
+    }
+    public static void displayPeople(people [] parray){
+        for (int i = 0; i <parray.length ; i++) {
+            parray[i].display();
+        }
+
+
+    }
+
 }
